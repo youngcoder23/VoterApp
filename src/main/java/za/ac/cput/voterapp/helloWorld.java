418312610b9b380ff915338140f3606deb28181a
219369151@mycput.ac.za <41047190+youngcoder23@users.noreply.github.com>
2021-10-26 22:15:01 +0100

    second commit: Created 2 simple programs; Hello World + Calculator

---
+++ src/main/java/za/ac/cput/voterapp/helloWorld.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.voterapp;
+
+/**
+ *
+ * @author musaddiq
+ * student number: 219369151
+ */
+public class helloWorld {
+    /* Simple Hello World */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    /*Simple sum calculation*/
+    public static int sum(final int a, final int b) {
+    return a + b;
+    }
+}
